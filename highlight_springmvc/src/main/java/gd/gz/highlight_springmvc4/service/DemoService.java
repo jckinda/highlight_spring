a67fdfdb08c0bb41cec2e7febba6012b64b3fb40
黄健达 <lsr@sina.com>
2016-08-10 00:09:48 +0800

    springmvc测试

---
+++ highlight_springmvc/src/main/java/gd/gz/highlight_springmvc4/service/DemoService.java
@@ -0,0 +1,10 @@
+package gd.gz.highlight_springmvc4.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+	public String saySonething() {
+		return "hello";
+	}
+}
